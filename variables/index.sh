0bb2288e7a6e76edb4f3421f9ed05d1e8ffff3ea
Noman <pxycknomdictator@outlook.com>
2025-05-18 02:14:53 +0500

    added a important and funny comment about bash

--- variables/index.sh
+++ variables/index.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# wait What its look like the mixer of Python and PHP
+
 username="Noman"
 age=69
+isChad=true
 
-echo "My name is $username and I am $age years old!"
+echo "My name is $username and I am $age years old and I am chad $isChad"
