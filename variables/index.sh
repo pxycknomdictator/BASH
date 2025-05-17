c8cbdaa0fa0a13a5e05f443d422d8d25d4c0058a
Noman <pxycknomdictator@outlook.com>
2025-05-18 02:38:49 +0500

    added comments to explain my script

--- variables/index.sh
+++ variables/index.sh
@@ -3,15 +3,19 @@
 echo "========== script started =========="
 echo "Processing User Information..."
 
+# sleep <t> means just stop your stupid script for <t> seconds
 sleep 2
 
+# I am just importing my .env variables from linux
 username=$USER
 host=$HOSTNAME
 age=69
 isChad=true
+# This is called command substitution that means do what ever inside this $() and assign it in variable
+date=$(date)
 kernel_version=$(uname -a)
 
-echo "My name is $username, I am $age years old, am chad = $isChad, and my hostname is $host"
+echo "Hey Its me $username and I am exposing my information on this date $date like my age $age. I am chad $isChad, and my hostname is $host"
 
 sleep 1
 
