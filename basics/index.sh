01661d00f92f16c9ce411de6066c2da53c699fd7
Noman <pxycknomdictator@outlook.com>
2025-05-17 18:19:19 +0500

    add how to print in shell

--- basics/index.sh
+++ basics/index.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Just print some message in shell not doing any big stuffs
+
 echo "======== Base Script is Running ========"
+
 echo "Loading your system scripts......" # HaHa
 echo "scripts is loaded"
-
 echo "Hello From Bash!"
 
+echo "======== Base Script is Ended ========"
