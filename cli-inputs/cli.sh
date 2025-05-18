00614f74b4bfc2d231fa963aa335e89bd8c16d58
Noman <pxycknomdictator@outlook.com>
2025-05-18 14:14:05 +0500

    added some stuffs in bash

--- cli-inputs/cli.sh
+++ cli-inputs/cli.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Are you want to die?"
+echo "========== script started =========="
+
+echo "Do You Like Bash (Y/N):"
 
 # This bad boy hold your script while user give his argument
 read command
 
+sleep 1
+
 echo "command executed: $command"
+
+echo "========== script ended =========="
+exit 0
