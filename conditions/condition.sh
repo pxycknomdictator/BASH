8a4684db86eaf4d927adcec2532dcefc589d1e17
Noman <pxycknomdictator@outlook.com>
2025-05-19 17:01:29 +0500

    added nested if/else

--- conditions/condition.sh
+++ conditions/condition.sh
@@ -2,10 +2,16 @@
 
 read -p "Which language Do you prefer for Project? (JS/TS/RUST): " language
 
-if [[ $language == "JS" ]]; then
-	echo "JavaScript Setup is initialized"
-elif [[ $language == "RUST" ]]; then
-    echo "Rust setup is initialized"
+# Check this $language is empty or not?
+
+if [[ -z $language ]]; then
+    echo "You didn't enter anything!"
 else
-	echo "TypeScript Setup is initialized"
+    if [[ $language == "JS" ]]; then
+        echo "JavaScript Setup is initialized"
+    elif [[ $language == "RUST" ]]; then
+        echo "Rust setup is initialized"
+    else
+        echo "TypeScript Setup is initialized"
+    fi
 fi
