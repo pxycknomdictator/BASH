3925d8d067372386ab8649153341ee37bc6b0e53
Noman <pxycknomdictator@outlook.com>
2025-05-21 12:45:31 +0500

    regular way to declare function body in bash

--- functions/func.sh
+++ functions/func.sh
@@ -10,5 +10,12 @@ greeting() {
     echo "Hello World"
 }
 
+# SAME SAME but Different XD
+
+function sayHello() {
+    echo "Hello Bash"
+}
+
 # Wait what this is how to call the functions????
 greeting
+sayHello
