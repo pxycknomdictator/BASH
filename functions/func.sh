303378053e2b7ee3c18ae94e0363bbe3e3d153e1
Noman <pxycknomdictator@outlook.com>
2025-05-21 12:43:54 +0500

    functions in bash script

---
+++ functions/func.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<< multiline_comment
+    Functions is a code that have same 
+    logic but with different arguments
+    blah blah blah
+multiline_comment
+
+greeting() {
+    echo "Hello World"
+}
+
+# Wait what this is how to call the functions????
+greeting
