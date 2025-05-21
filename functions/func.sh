62ab4fcbcf52f7c7169490f76f852d7d598946b7
Noman <pxycknomdictator@outlook.com>
2025-05-21 14:28:06 +0500

    added auth method in bash

--- functions/func.sh
+++ functions/func.sh
@@ -10,16 +10,22 @@ greetings() {
     echo "Hello I am $name, and I am $age Years Old!"
 }
 
+authentication() {
+    local name=$1
+    local age=$2
+
+    if [[ -z "$name" || -z "$age" ]]; then
+        echo -e "\e[1;31m[Error]\e[0m Please enter your name and age as an argument!"
+        echo -e "\e[1;33mUsage:\e[0m ./script.sh YourName age"
+        exit 1
+    fi    
+}
+
 echo -e "\n\e[1;35m========== BASH SCRIPT IS STARTING ==========\e[0m"
 sleep 1
 
 name=$1
 age=$2
 
-if [[ -z "$name" || -z "$age" ]]; then
-    echo -e "\e[1;31m[Error]\e[0m Please enter your name and age as an argument!"
-    echo -e "\e[1;33mUsage:\e[0m ./script.sh YourName age"
-    exit 1
-fi
-
+authentication "$name" "$age"
 greetings "$name" "$age"
